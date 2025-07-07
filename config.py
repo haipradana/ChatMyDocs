b99002d73d255f0bb61448bd24bba9d33db7b989
Pradana Yahya Abdillah <pradanayahyaabdillah@mail.ugm.ac.id>
2025-07-07 11:13:35 +0700

    update config

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+OPENAI_BASE_URL = os.getenv("OPENAI_BASE_URL")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
--- engine/llm_openai.py
+++ engine/llm_openai.py
@@ -3,13 +3,14 @@ import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from llama_index.llms.openai import OpenAI
-from config import OPENAI_API_KEY
+from config import OPENAI_API_KEY, OPENAI_BASE_URL
 
 def load_llm():
     return OpenAI(
         model="gpt-4.1-mini",
         temperature=0.0,
         api_key=OPENAI_API_KEY,
+        base_url=OPENAI_BASE_URL,
         max_tokens=1024,
         stream=True,
     )
